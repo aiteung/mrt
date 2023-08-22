d03a7173d99a41ff795634354091733ca3aab9a5
Rolly Maulana Awangga <awangga@gmail.com>
2023-08-23 04:02:32 +0700

    run with compat v1

--- run.go
+++ run.go
@@ -4,11 +4,16 @@ import (
 	"github.com/aiteung/module/model"
 )
 
-func Run(Pesan model.IteungMessage, DBIface model.IteungDBConfig, URLApiWa string) (Modulename string) {
+func Run(Pesan model.IteungMessage, DBIface model.IteungDBConfig, URLApiWa string, HostIteungV1 string, apikeyv1 string) (Modulename string) {
 	NormalizeAndTypoCorrection(&Pesan.Message, DBIface.MongoConn, DBIface.TypoCollection)
 	if IsIteungCall(Pesan) {
 		Modulename = GetModuleName(Pesan, DBIface.MongoConn, DBIface.ModuleCollection)
 	}
-	go CallAndSend(Modulename, Pesan, URLApiWa)
+	if Modulename != "" {
+		CallAndSend(Modulename, Pesan, URLApiWa)
+	} else {
+		IteungV1(HostIteungV1, apikeyv1, Pesan)
+	}
+
 	return
 }
---
+++ v1.go
@@ -0,0 +1,31 @@
+package module
+
+import (
+	"github.com/aiteung/atapi"
+	"github.com/aiteung/atmessage"
+	"github.com/aiteung/module/model"
+)
+
+func IteungV1(IteungIPAddress string, apikey string, Pesan model.IteungMessage) {
+	var im atmessage.IteungMessage
+	im.Phone_number = Pesan.Phone_number
+	im.Group_name = Pesan.Group_name + "@" + Pesan.Chat_number
+	im.Alias_name = Pesan.Alias_name
+	im.Messages = Pesan.Message
+	if Pesan.Is_group {
+		im.Is_group = "true"
+	} else {
+		im.Is_group = "false"
+	}
+	im.Filename = Pesan.Filename
+	im.Filedata = Pesan.Filedata
+	im.Latitude = Pesan.Latitude
+	im.Longitude = Pesan.Longitude
+	im.Api_key = apikey
+	SendToIteungV1API(IteungIPAddress, im)
+}
+
+func SendToIteungV1API(IteungIPAddress string, pesan atmessage.IteungMessage) atmessage.IteungRespon {
+	urltarget := "http://" + IteungIPAddress + "/iteung/chatbot"
+	return atapi.PostStruct[atmessage.IteungRespon](pesan, urltarget)
+}
