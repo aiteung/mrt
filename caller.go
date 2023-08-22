3b195d7c113ae9858b5e0aa733fed2abcaec015b
Rolly Maulana  Awangga <rolly@awang.ga>
2023-08-22 12:08:33 +0700

    return err

--- caller.go
+++ caller.go
@@ -4,6 +4,7 @@ import (
 	"github.com/aiteung/atmessage"
 	"github.com/aiteung/module/handler"
 	"github.com/aiteung/module/model"
+	"go.mau.fi/whatsmeow"
 )
 
 func Caller(Modulename string, Pesan model.IteungMessage) (reply string) {
@@ -14,7 +15,8 @@ func Caller(Modulename string, Pesan model.IteungMessage) (reply string) {
 	return
 }
 
-func CallAndSend(Modulename string, Pesan model.IteungMessage, WAIface model.IteungWhatsMeowConfig) {
+func CallAndSend(Modulename string, Pesan model.IteungMessage, WAIface model.IteungWhatsMeowConfig) (resp whatsmeow.SendResponse, err error) {
 	reply := Caller(Modulename, Pesan)
-	atmessage.SendMessage(reply, WAIface.Info.Chat, WAIface.Waclient)
+	resp, err = atmessage.SendMessage(reply, WAIface.Info.Chat, WAIface.Waclient)
+	return
 }
