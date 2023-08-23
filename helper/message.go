7783070739c0529e27abb414f2e6b686e3abd1c1
Rolly Maulana  Awangga <rolly@awang.ga>
2023-08-23 11:25:15 +0700

    cek EntryPointConversionSource

--- helper/message.go
+++ helper/message.go
@@ -85,8 +85,10 @@ func GetFile(Message *waProto.Message) (filename, filedata string) {
 func GetStatusFromLink(WAIface model.IteungWhatsMeowConfig) (whmsg bool) {
 	if WAIface.Message.ExtendedTextMessage != nil && WAIface.Info.Chat.Server == "s.whatsapp.net" {
 		if WAIface.Message.ExtendedTextMessage.ContextInfo != nil {
-			if *WAIface.Message.ExtendedTextMessage.ContextInfo.EntryPointConversionSource == "click_to_chat_link" {
-				whmsg = true
+			if *WAIface.Message.ExtendedTextMessage.ContextInfo.EntryPointConversionSource != "" {
+				if *WAIface.Message.ExtendedTextMessage.ContextInfo.EntryPointConversionSource == "click_to_chat_link" {
+					whmsg = true
+				}
 			}
 		}
 	}
